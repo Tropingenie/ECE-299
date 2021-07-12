38ea88b2f5d6c1607d7a7defdc63de4a2922ed44
Benjamin Lyne <steeevie22@gmail.com>
2021-07-11 21:54:20 -0700

    Initial Commit

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/LED_Control.c \
+../src/clock_control.c \
+../src/initialize-hardware.c \
+../src/main.c \
+../src/stm32f4xx_hal_msp.c \
+../src/write.c 
+
+OBJS += \
+./src/LED_Control.o \
+./src/clock_control.o \
+./src/initialize-hardware.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/write.o 
+
+C_DEPS += \
+./src/LED_Control.d \
+./src/clock_control.d \
+./src/initialize-hardware.d \
+./src/main.d \
+./src/stm32f4xx_hal_msp.d \
+./src/write.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/stm32f4xx_hal_msp.o: ../src/stm32f4xx_hal_msp.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-padded -Wno-missing-prototypes -Wno-missing-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/stm32f4xx_hal_msp.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ src/clockface.h
@@ -0,0 +1,13 @@
+#ifndef clock_control
+#define clock_control
+
+struct ClockFaceTag;
+
+typedef struct ClockFaceTag{
+		unsigned h1;
+		unsigned h2;
+		unsigned m1;
+		unsigned m2;
+} ClockFace;
+
+#endif
